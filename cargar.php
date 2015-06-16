b36613f0f9d3ac0d87ef36bf6dbe84a491e03180
Mache <mache.uno@gmail.com>
2015-06-16 16:32:22 -0300

    fin carga

--- cargar.php
+++ cargar.php
@@ -2,6 +2,6 @@
 require 'monster.php';
 $__MON		= new Monster();
 //$__HEAD		= array('CIRCUITO', 'SECCION', 'DESCRIPCION', 'MUNICIPIO', 'DISTRITO', 'CIRC_NOD_1', 'CIRC_NOD_2', 'CIRC_NOD_3', 'CIRC_NOD_4');
-$__MON->LOADCSV('PADRON-JUN2015/tmun.txt','municipio','|','AUTO');
+//$__MON->LOADCSV('PADRON-JUN2015/KOLINA.txt','afiliados','|','AUTO');
 
 ?>
